6f8878a80555d6b442c7307d9538ce3c28570d4c
shobhi10058 <rkgupta462gkp@gmail.com>
2021-10-12 18:46:15 +0530

    added internal indices in task_2.py

--- task_2.py
+++ task_2.py
@@ -9,6 +9,16 @@ k = int(input())
 init_centers = k_means.random_centers(k, list_of_data)
 clusters = k_means.k_means(init_centers, list_of_data)
 y_label, y_pred = k_means.get_predictions(clusters)
+
+# metrics with ground truth
 print(sklearn.metrics.homogeneity_score(y_label, y_pred))
 print(sklearn.metrics.normalized_mutual_info_score(y_label, y_pred))
 print(sklearn.metrics.adjusted_rand_score(y_label, y_pred))
+
+X = []
+for cluster in clusters:
+	for data in cluster:
+		X.append(data)
+		
+# metrics without ground truth
+print(sklearn.metrics.silhouette_score(X, y_pred))
